c499144befc935fbee7ad0ad788a4b5640657ee2
Maka5000 <mak.khas01@gmail.com>
2023-01-10 15:00:10 +0600

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let NumToArray = n.toString().split('');
+    let reversedArray = [];
+
+    for(let i = NumToArray.length -1; i >= 0; i--) {
+        if (NumToArray[i] != '-'){
+            reversedArray.push(NumToArray[i]);
+        }
+    };
+
+    let ArrayToNumber = parseInt(reversedArray.join(''));
+    
+    return ArrayToNumber;
 }
